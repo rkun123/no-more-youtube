c8da97810e25135f4d7272608f3ee1407a75e6b1
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-03-13 22:52:57 +0900

    [update] descriptionのリンクをaタグに変換

--- pages/video/_id.vue
+++ pages/video/_id.vue
@@ -2,10 +2,10 @@
   <div>
     <video-player :video-id="$route.params.id" />
     <div class="title">
-      <h1>{{ video.title }}</h1>
+      <h2>{{ video.title }}</h2>
     </div>
     <div class="description">
-      <div v-html="video.description"></div>
+      <p v-html="textLink(this.video.description)" />
     </div>
   </div>
 </template>
@@ -15,6 +15,7 @@ import Vue from 'vue'
 import VideoPlayer from '~/components/Video/VideoPlayer.vue'
 
 export default Vue.extend({
+  components: { VideoPlayer },
   data () {
     return {
       params: {
@@ -28,7 +29,6 @@ export default Vue.extend({
       }
     }
   },
-  components: { VideoPlayer },
   created () {
     this.params.id = this.$route.params.id
     this.params.key = String(process.env.YOUTUBE_API_KEY)
@@ -40,8 +40,32 @@ export default Vue.extend({
         const data = result.data.items[0].snippet
         this.video.title = data.title
         this.video.description = data.description
-        console.log(this.video)
+        console.log(result)
       })
+  },
+  methods: {
+    textLink (text: string) {
+      // eslint-disable-next-line camelcase
+      const regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g
+      // eslint-disable-next-line camelcase
+      const regexp_makeLink = function (url: string, href: string) {
+        return '<a href="h' + href + '">' + url + '</a>'
+      }
+      return text.replace(regexp_url, regexp_makeLink)
+    }
   }
 })
 </script>
+
+<style scoped>
+.title {
+  margin-left: 10px;
+}
+.description {
+  margin-top: 20px;
+  margin-left: 20px;
+  white-space: pre-wrap;
+  word-wrap:break-word;
+  color: #030303;
+}
+</style>
